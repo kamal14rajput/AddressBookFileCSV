e36e32c7f4fbd8425e0aebc6d6173e7bf98d79ea
kamal14rajput <kamal14rajput@gmail.com>
2022-01-19 01:34:18 +0530

    UPDATED: read and write as csv file

--- src/main/java/com/bridgelabz/InterfaceOne.java
+++ src/main/java/com/bridgelabz/InterfaceOne.java
@@ -1,24 +1,49 @@
 package com.bridgelabz;
 
+import java.io.IOException;
 import java.util.List;
+
+import com.opencsv.exceptions.CsvDataTypeMismatchException;
+import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 
 public interface InterfaceOne {
 	void createContact();
+
 	void addContact();
+
 	void editContact();
+
 	void deleteContact();
+
 	void display();
+
 	void displayBook();
+
 	void addToBook();
+
 	void operation();
+
 	void searchPerson();
+
 	void addContactByCity();
+
 	void viewByCity();
+
 	void addContactByState();
+
 	void viewByState();
+
 	void sortAddressBook();
+
 	void sortByCity();
+
 	void sortByState();
+
 	void writeToAddressBookFile();
+
 	public List<String> readDataFromFile();
+
+	public void writeDataToCSV() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException;
+
+	public <CsvValidationException extends Throwable> void readDataFromCSV() throws IOException, CsvValidationException;
 }
